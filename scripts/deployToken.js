959d95c1c514823316348fb6a69de5d781320f0a
opcc <opcc22059@gmail.com>
2022-10-31 17:03:08 +0800

    update

--- scripts/deployToken.js
+++ scripts/deployToken.js
@@ -1,11 +1,17 @@
 const hardhat = require("hardhat");
 
+const net = process.env.HARDHAT_NETWORK
+const v = process.argv
+const name = v[2];
+const symbol = v[3];
+const decimal = v[4];
+
 async function main() {
     
     const Token = await hardhat.ethers.getContractFactory("TestToken");
   
     console.log("Deploying .....")
-    const token = await Token.deploy();
+    const token = await Token.deploy(name, symbol, decimal);
     await token.deployed();
     console.log("Token Contract Address: " , token.address);
   
